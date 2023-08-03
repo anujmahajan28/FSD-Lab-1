4f364f32f576c7b94004a722c2f0051c42e59fcf
anujmahajan28 <141295015+anujmahajan28@users.noreply.github.com>
2023-08-03 10:20:56 +0530

    min fsdl1.py

--- fsdl1.py
+++ fsdl1.py
@@ -1,4 +1,4 @@
 list = [0,1,2,3,4,5,6,7,8,9]
-
-print('List elements are: ", list, end="\n\n")
-print("Maximum element in list is: ",max(list))
+print("List elements are: ", list, end="\n\n")
+print("Maximum element in the list is: ",max(list))
+print("Minimum element in the list is: ",min(list))
